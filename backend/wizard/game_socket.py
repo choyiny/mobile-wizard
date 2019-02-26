69e421c3e7feb6e6ed4a3b1b581a7d7ad830cce9
Yong Cho Yin <choyiny@users.noreply.github.com>
2019-02-26 15:16:03 -0500

    fix sockets connection

--- backend/wizard/game_socket.py
+++ backend/wizard/game_socket.py
@@ -6,4 +6,4 @@ class Game(Namespace):
 
     def on_action(self, data):
         """ Method is called when an action is sent from the phone. """
-        emit('echo', data)
+        emit('echo', data, broadcast=True)
