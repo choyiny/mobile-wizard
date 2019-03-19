6f15903114f38987cf567779a2eb06cba8556816
Yong Cho Yin <choyiny@users.noreply.github.com>
2019-03-19 17:01:36 -0400

    [frontend] style results page

--- frontend/src/app/host/host-routing.module.ts
+++ frontend/src/app/host/host-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GameComponent} from './game/game.component';
-import {GameGuard} from '../helpers/game.guard';
 import {RoomLobbyComponent} from './room-lobby/room-lobby.component';
 import {ResultComponent} from './result/result.component';
 
